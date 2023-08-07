db311f078acfd1e97e0523d63a784234933694fd
Geoff Liu <hi@geoffliu.me>
2023-08-07 12:04:33 +0800

    Disable Stunfork

--- src/ruleengine/dsl/actions.py
+++ src/ruleengine/dsl/actions.py
@@ -5,24 +5,3 @@ class Action(ABC):
     def run(self, item, scope):
         pass
 
-
-class PrintAction(Action):
-    def __init__(self, target_dir, title, description):
-        self.target_dir = target_dir
-        self.title = title
-        self.description = description
-
-    def run(self, item, scope):
-        json_object = {
-            "title": self.title,
-            "description": self.description,
-            "timestamp": item.ts,
-            "fileList": [item.topic]
-        }
-        with open(os.path.join(self.target_dir, str(uuid.uuid4()) + ".json"), "w") as f:
-            print(f"==> Creating a moment: {self.title}")
-            json.dump(json_object, f)
-
-
-def create_moment(title, description):
-    return PrintAction("/root/.ros/auto-upload", title, description)
