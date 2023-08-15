0e092b584c5b470bc8aed7945e2c672af16d11bc
Geoff Liu <hi@geoffliu.me>
2023-08-15 11:58:14 +0800

    Grease Pans

--- src/ruleengine/dsl/log_conditions.py
+++ src/ruleengine/dsl/log_conditions.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from .base_conditions import *
 
 # TODO: Add tests
@@ -5,6 +6,8 @@ from .base_conditions import *
 
 _is_foxglove = or_(type_is('foxglove_msgs/Log'), type_is('foxglove.Log'))
 _is_ros = type_is('rosgraph_msgs/Log')
+
+LogLevel = Enum('LogLevel', ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'])
 
 log_text = or_(
     and_(_is_ros, msg.msg),
@@ -19,25 +22,25 @@ log_level = or_(
     and_(_is_foxglove, msg.map_condition_value(lambda m: foxglove_log_level(m.level))),
 
     # Default case
-    'UNKNOWN',
+    LogLevel.UNKNOWN,
 )
 
 def ros_log_level(num):
     match num:
-        case 1: return 'DEBUG'
-        case 2: return 'INFO'
-        case 4: return 'WARN'
-        case 8: return 'ERROR'
-        case 16: return 'FATAL'
-        _: return 'UNKNOWN'
+        case 1: return LogLevel.DEBUG
+        case 2: return LogLevel.INFO
+        case 4: return LogLevel.WARN
+        case 8: return LogLevel.ERROR
+        case 16: return LogLevel.FATAL
+        _: return LogLevel.UNKNOWN
 
 
 def foxglove_log_level(num):
     match num:
-        case 1: return 'DEBUG'
-        case 2: return 'INFO'
-        case 3: return 'WARN'
-        case 4: return 'ERROR'
-        case 5: return 'FATAL'
-        _: return 'UNKNOWN'
+        case 1: return LogLevel.DEBUG
+        case 2: return LogLevel.INFO
+        case 3: return LogLevel.WARN
+        case 4: return LogLevel.ERROR
+        case 5: return LogLevel.FATAL
+        _: return LogLevel.UNKNOWN
 
