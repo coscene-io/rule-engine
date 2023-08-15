722eefa53f12b0872cd3a38ad82cb3dee6a212ff
Geoff Liu <hi@geoffliu.me>
2023-08-15 11:40:48 +0800

    Submerge Servohook

--- src/ruleengine/dsl/util_conditions.py
+++ src/ruleengine/dsl/util_conditions.py
@@ -1,10 +1,11 @@
-from .condition import Condition
 from .base_conditions import *
 
 log_text = or_(
     and_(type_is('foxglove_msgs/Log'), msg.message),
     and_(type_is('foxglove.Log'), msg.message),
     and_(type_is('rosgraph_msgs/Log'), msg.msg),
-    Condition.wrap(''),
+
+    # Always have a default case of empty string
+    '',
 )
 
