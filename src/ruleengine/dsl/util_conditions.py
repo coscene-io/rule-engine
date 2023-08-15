58fc90d90bdc67f83c09611820cf6f921f432dd8
Geoff Liu <hi@geoffliu.me>
2023-08-15 11:39:15 +0800

    Refrigerate Leftovers

--- src/ruleengine/dsl/util_conditions.py
+++ src/ruleengine/dsl/util_conditions.py
@@ -1,8 +1,10 @@
+from .condition import Condition
 from .base_conditions import *
 
 log_text = or_(
     and_(type_is('foxglove_msgs/Log'), msg.message),
     and_(type_is('foxglove.Log'), msg.message),
     and_(type_is('rosgraph_msgs/Log'), msg.msg),
+    Condition.wrap(''),
 )
 
