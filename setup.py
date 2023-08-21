d3a86ff59accd1ccde3e886a66c228ab8069d601
Shuhao Qing <shuhao.qing@coscene.io>
2023-08-21 19:32:52 +0800

    nit

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
+import logging
 import os
 import re
 
 from setuptools import setup, find_packages
 
-version = os.environ.get('RULE_ENGINE_VERSION')
+version = os.environ.get("RULE_ENGINE_VERSION")
 if not version:
-    raise ValueError('RULE_ENGINE_VERSION is not set')
+    logging.info("RULE_ENGINE_VERSION not set, might be running in CI")
+    version = "0.0.0"
 
-version = re.sub('^v', '', version)
-if not re.match(r'^\d+\.\d+\.\d+(-(a|b|rc)\d+)?$', version):
-    raise ValueError(f'Invalid version: {version}')
+version = re.sub("^v", "", version)
+if not re.match(r"^\d+\.\d+\.\d+(-(a|b|rc)\d+)?$", version):
+    raise ValueError(f"Invalid version: {version}")
 
 
-def requires(filename='requirements.txt'):
+def requires(filename="requirements.txt"):
     with open(filename) as f:
         return f.read().splitlines()
 
@@ -24,8 +26,6 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=requires(),
-    extras_require={
-        "dev": requires('requirements-dev.txt')
-    },
+    extras_require={"dev": requires("requirements-dev.txt")},
     python_requires=">=3.7",
 )
