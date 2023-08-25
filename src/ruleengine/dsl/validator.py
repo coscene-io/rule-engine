c1782c72e0d787dbad4a193905b69e1bb3a84df0
Geoff Liu <hi@geoffliu.me>
2023-08-25 18:11:36 +0800

    Heat X-vents Leg

--- src/ruleengine/dsl/validator.py
+++ src/ruleengine/dsl/validator.py
@@ -3,6 +3,7 @@ from ruleengine import dsl
 
 base_dsl_values = dict(inspect.getmembers(dsl))
 
+
 def upload(
     before=10,
     title="",
@@ -11,6 +12,7 @@ def upload(
     extra_files=[],
 ):
     pass
+
 
 def create_moment(
     title,
@@ -22,15 +24,17 @@ def create_moment(
 ):
     pass
 
+
 actions_dsl_values = {
-        'upload': upload,
-        'create_moment': create_moment,
-        **base_dsl_values
-        }
+    "upload": upload,
+    "create_moment": create_moment,
+    **base_dsl_values,
+}
+
 
 def validate_condition(cond_str):
     eval(cond_str, base_dsl_values)
 
+
 def validate_action(action_str):
     eval(action_str, actions_dsl_values)
-
