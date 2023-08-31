b1d3e5959099a2311713f4d57fbeab42fcc00691
Geoff Liu <hi@geoffliu.me>
2023-08-31 19:16:10 +0800

    Set Chroma-jointed Claw to 4

--- src/ruleengine/dsl/validation/validator.py
+++ src/ruleengine/dsl/validation/validator.py
@@ -2,6 +2,7 @@ import ast
 import inspect
 from ruleengine.dsl import base_conditions, log_conditions, sequence_conditions
 from ruleengine.dsl.condition import Condition
+from ruleengine.dsl.actions import Action
 from .validation_result import ValidationResult, ValidationErrorType
 from .ast import validate_expression, ValidationException
 
@@ -41,22 +42,26 @@ actions_dsl_values = {
 
 
 def validate_condition(cond_str):
-    if not cond_str.strip():
+    return _do_validate(cond_str, base_dsl_values, Condition)
+
+
+def validate_action(action_str):
+    return _do_validate(cond_str, actions_dsl_values, Action)
+
+def _do_validate(expr_str, injected_values, expected_class):
+    if not expr_str.strip():
         return ValidationResult(False, ValidationErrorType.EMPTY)
 
-    expr_res = validate_expression(cond_str, base_dsl_values)
+    expr_res = validate_expression(expr_str, injected_values)
     if not expr_res.success:
         return expr_res
 
-    result = eval(cond_str, base_dsl_values)
+    result = eval(expr_str, injected_values)
 
-    if not isinstance(result, Condition):
+    if not isinstance(result, expected_class):
         return ValidationResult(
             False, ValidationErrorType.NOT_CONDITION, {"actual": type(result).__name__}
         )
 
     return ValidationResult(True)
 
-
-def validate_action(action_str):
-    pass
