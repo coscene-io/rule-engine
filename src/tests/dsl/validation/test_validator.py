8ca4d599bd12c16ec63d1f3b9b7680508caa7082
Geoff Liu <hi@geoffliu.me>
2023-08-31 19:12:37 +0800

    Collect Mail

--- src/tests/dsl/validation/test_validator.py
+++ src/tests/dsl/validation/test_validator.py
@@ -6,40 +6,40 @@ from ruleengine.dsl.validation.validation_result import ValidationErrorType
 
 class ValidatorTest(unittest.TestCase):
     def test_condition_validation(self):
-        # self.assertTrue(validate_condition("msg").success)
-        # self.assertTrue(validate_condition("topic_is('blah')").success)
-        # self.assertTrue(validate_condition("and_(topic_is('blah'), msg.something == 123)").success)
+        self.assertTrue(validate_condition("msg").success)
+        self.assertTrue(validate_condition("topic_is('blah')").success)
+        self.assertTrue(validate_condition("and_(topic_is('blah'), msg.something == 123)").success)
 
-        # c = validate_condition("    ")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.EMPTY)
+        c = validate_condition("    ")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.EMPTY)
 
-        # c = validate_condition("123")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.NOT_CONDITION)
-        # self.assertEqual(c.details["actual"], "int")
+        c = validate_condition("123")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.NOT_CONDITION)
+        self.assertEqual(c.details["actual"], "int")
 
-        # c = validate_condition("'hello'")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.NOT_CONDITION)
-        # self.assertEqual(c.details["actual"], "str")
+        c = validate_condition("'hello'")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.NOT_CONDITION)
+        self.assertEqual(c.details["actual"], "str")
 
-        # c = validate_condition("return 1")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
+        c = validate_condition("return 1")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
 
-        # c = validate_condition("msg.123")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
+        c = validate_condition("msg.123")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
 
-        # c = validate_condition("msg.blah(   ")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
+        c = validate_condition("msg.blah(   ")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
 
-        # c = validate_condition("missing_function(123)")
-        # self.assertFalse(c.success)
-        # self.assertEqual(c.error_type, ValidationErrorType.UNDEFINED)
-        # self.assertEqual(c.details["name"], "missing_function")
+        c = validate_condition("missing_function(123)")
+        self.assertFalse(c.success)
+        self.assertEqual(c.error_type, ValidationErrorType.UNDEFINED)
+        self.assertEqual(c.details["name"], "missing_function")
 
         # Actions should not be part of condition
         c = validate_condition("create_moment('hello')")
