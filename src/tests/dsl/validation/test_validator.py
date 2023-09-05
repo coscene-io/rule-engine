acd404f60c7e407cc9362e4e53615a12eaa367fb
Geoff Liu <hi@geoffliu.me>
2023-09-05 17:00:49 +0800

    Pay Speeding Fines

--- src/tests/dsl/validation/test_validator.py
+++ src/tests/dsl/validation/test_validator.py
@@ -89,9 +89,7 @@ class ValidatorTest(unittest.TestCase):
             ).success
         )
 
-        c = validate_action(
-            "create_moment('hello', description='', duration=1, 'something')", noop
-        )
+        c = validate_action("create_moment('hello', duration='', 'something')", noop)
         self.assertFalse(c.success)
         self.assertEqual(c.error_type, ValidationErrorType.SYNTAX)
 
