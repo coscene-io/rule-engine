b90ff6764d3bdfc4477468561f4b0647eae6fa8d
Geoff Liu <hi@geoffliu.me>
2023-09-07 18:58:01 +0800

    Turn on Parabolic Hexnetwork

--- src/ruleengine/dsl/validation/normalizer.py
+++ src/ruleengine/dsl/validation/normalizer.py
@@ -30,6 +30,27 @@ class BooleanTransformer(ast.NodeTransformer):
     def visit_Compare(self, node):
         node = self.generic_visit(node)
 
+        # We need to jump through quite a few hoops to keep inline with Python's
+        # semantics for comparison operators, just so we can rewrite the `in`
+        # operator into our `has` function call. Suppose you have the expression
+        #
+        #  a op1 b op2 c
+        #
+        # Python treats this as
+        #
+        #  a op1 b and b op2 c
+        #
+        # Except `b` must only be evaluated once. So we package the above into
+        # the following:
+        #
+        # (lambda arg0, arg1, arg2:
+        #   and_(
+        #     arg0 op1 arg1,
+        #     arg1 op2 arg2))(a, b, c)
+        #
+        # And if one of the operators happen to be an `in` operator, we swap it
+        # out with a `has` call.
+
         args = [node.left] + node.comparators
         param_list = ["arg" + str(i) for i in range(len(args))]
         condition_list = []
