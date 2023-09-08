998ef2bfb78729f4aaa303f696aac40357873cc2
Geoff Liu <hi@geoffliu.me>
2023-09-08 17:53:13 +0800

    ASTEROID! (everybody shake)

--- src/tests/dsl/test_condition.py
+++ src/tests/dsl/test_condition.py
@@ -2,6 +2,7 @@ import unittest
 
 from ruleengine.dsl.condition import Condition
 
+
 class ConditionTest(unittest.TestCase):
     def test_arithmetics(self):
         self.assertEqual(-4, int(self._get_eval_result(-Condition.wrap(4))))
@@ -19,7 +20,5 @@ class ConditionTest(unittest.TestCase):
         self.assertEqual(2, int(self._get_eval_result(Condition.wrap(12) / 6)))
         self.assertEqual(2, int(self._get_eval_result(12 / Condition.wrap(6))))
 
-
     def _get_eval_result(self, cond):
         return cond.evaluate_condition_at(None, {})[0]
-
