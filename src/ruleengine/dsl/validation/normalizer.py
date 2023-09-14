14f74f2d0325cce3f8e071946834297fd90385cf
Geoff Liu <hi@geoffliu.me>
2023-09-14 16:10:38 -0500

    Rotate Star-bellied Nanojig

--- src/ruleengine/dsl/validation/normalizer.py
+++ src/ruleengine/dsl/validation/normalizer.py
@@ -6,6 +6,30 @@ def normalize_expression_tree(tree):
 
 
 class BooleanTransformer(ast.NodeTransformer):
+    def visit_JoinedStr(self, node):
+        node = self.generic_visit(node)
+
+        new_values = []
+        condition_args = []
+        for v in node.values:
+            match v:
+                case ast.Constant(value):
+                    new_values.append(v)
+                case ast.FormattedValue(value, conversion, format_spec):
+                    condition_args.append(value)
+                    new_values.append(ast.FormattedValue(ast.Name(f'arg{len(args_map)}', ast.Load()), conversion, format_spec))
+                case _:
+                    raise Exception(f"Shouldn't get here: {v}")
+
+        lambda_node = self._eval_expr(f"lambda {','.join(f'arg{i}' for i in range(len(condition_args)))}: ...")
+        lambda_node.body = ast.JoinedStr(new_values)
+        return ast.Call(
+                ast.Name('func_apply', ast.Load()),
+                condition_args,
+                []
+                )
+
+
     def visit_BoolOp(self, node):
         node = self.generic_visit(node)
 
@@ -57,13 +81,9 @@ class BooleanTransformer(ast.NodeTransformer):
         for i, (left, right) in enumerate(zip(param_list, param_list[1:])):
             match node.ops[i]:
                 case ast.In():
-                    condition_list.append(
-                        ast.parse(f"has({right}, {left})", mode="eval").body
-                    )
+                    condition_list.append( self._eval_expr(f"has({right}, {left})"))
                 case ast.NotIn():
-                    condition_list.append(
-                        ast.parse(f"not_(has({right}, {left}))", mode="eval").body
-                    )
+                    condition_list.append( self._eval_expr(f"not_(has({right}, {left}))"))
                 case _:
                     condition_list.append(
                         ast.Compare(
@@ -73,7 +93,10 @@ class BooleanTransformer(ast.NodeTransformer):
                         )
                     )
 
-        wrapper = ast.parse(f"lambda {','.join(param_list)}: ...", mode="eval").body
+        wrapper = self._eval_expr(f"lambda {','.join(param_list)}: ...")
         wrapper.body = ast.Call(ast.Name("and_", ast.Load()), condition_list, [])
 
         return ast.Call(wrapper, args, [])
+
+    def _eval_expr(self, str_value):
+        return ast.parse(str_value, mode="eval").body
