ba7a48a18558692f3a155f4ab25bd210c29c2385
Shuhao Qing <shuhao.qing@coscene.io>
2023-10-24 16:02:56 +0800

    improve engine

--- src/ruleengine/engine.py
+++ src/ruleengine/engine.py
@@ -21,14 +21,41 @@ class Rule:
 
 
 class Engine:
-    def __init__(self, rules):
+    def __init__(self, rules, should_trigger_action=lambda *x: True, trigger_cb=None):
         self.__rules = rules
+        self.__should_trigger_action = should_trigger_action
+        self.__trigger_cb = trigger_cb
 
     def consume_next(self, item):
         for rule in self.__rules:
-            for cond in rule.conditions:
+            triggered_condition_indices = []
+            triggered_scope = None
+
+            for i, cond in enumerate(rule.conditions):
                 res, scope = cond.evaluate_condition_at(item, rule.initial_scope)
                 if res:
-                    for action in rule.actions:
-                        action.run(item, scope)
-                    break
+                    triggered_condition_indices.append(i)
+                if not triggered_scope:
+                    triggered_scope = scope
+
+            if not triggered_condition_indices:
+                continue
+
+            # For testing, rule.spec is not specified
+            hit = (
+                {}
+                if not rule.spec
+                else {
+                    **rule.spec,
+                    "when": [rule.spec["when"][i] for i in triggered_condition_indices],
+                }
+            )
+
+            action_triggered = False
+            if self.__should_trigger_action(rule.project_name, rule.spec, hit):
+                action_triggered = True
+                for action in rule.actions:
+                    action.run(item, triggered_scope)
+
+            if self.__trigger_cb:
+                self.__trigger_cb(rule.project_name, rule.spec, hit, action_triggered)
