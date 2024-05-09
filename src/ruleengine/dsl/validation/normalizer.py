80476a411788949a46a78e8085671e54f6f7c9db
fei.gao <fei.gao@coscene.io>
2024-05-09 17:53:42 +0800

    comments

--- src/ruleengine/dsl/validation/normalizer.py
+++ src/ruleengine/dsl/validation/normalizer.py
@@ -14,6 +14,10 @@ class BooleanTransformer(ast.NodeTransformer):
                 new_values.append(v)
             elif isinstance(v, ast.FormattedValue):
                 if v.format_spec:
+                    # In theory, f-strings format spec can be another
+                    # f-string, but it makes our logic very clumsy, so we
+                    # just don't support it for now. Check here that it's
+                    # only a constant.
                     if len(v.format_spec.values) != 1 or not isinstance(
                         v.format_spec.values[0], ast.Constant
                     ):
@@ -56,6 +60,12 @@ class BooleanTransformer(ast.NodeTransformer):
             return node
 
     def visit_Call(self, node):
+        # A hack since 1. conditions have states, and 2. within SequenceMatchCondition,
+        # we need to create clean copies of the condition objects to avoid state
+        # sharing. As a result, we need to transform the condition arguments
+        # of the SequenceMatchCondition related functions to condition factories,
+        # with which we can lazy create copies of the condition objects
+        # within the SequenceMatchCondition.
         node = self.generic_visit(node)
         if isinstance(node.func, ast.Name) and node.func.id in ("repeated", "debounce"):
             factory = self._eval_expr("lambda: ...")
@@ -71,6 +81,27 @@ class BooleanTransformer(ast.NodeTransformer):
         return node
 
     def visit_Compare(self, node):
+        # We need to jump through quite a few hoops to keep inline with Python's
+        # semantics for comparison operators, just so we can rewrite the `in`
+        # operator into our `has` function call. Suppose you have the expression
+        #
+        #  a op1 b op2 c
+        #
+        # Python treats this as
+        #
+        #  a op1 b and b op2 c
+        #
+        # Except `b` must only be evaluated once. So we package the above into
+        # the following:
+        #
+        # (lambda arg0, arg1, arg2:
+        #   and_(
+        #     arg0 op1 arg1,
+        #     arg1 op2 arg2))(a, b, c)
+        #
+        # And if one of the operators happen to be an `in` operator, we swap it
+        # out with a `has` call.
+
         node = self.generic_visit(node)
         args = [node.left] + node.comparators
         param_list = ["arg" + str(i) for i in range(len(args))]
